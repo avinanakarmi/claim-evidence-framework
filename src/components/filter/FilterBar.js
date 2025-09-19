935e3b3459d34af84a56006fa2878531fda26042
avinanakarmi <avinanakarmi5@gmail.com>
2025-09-19 08:55:33 -0400

    functional filter

--- src/components/filter/FilterBar.js
+++ src/components/filter/FilterBar.js
@@ -1,3 +1,4 @@
+import React from "react";
 import FilterItem from "./FilterItem";
 
 const filters = {
@@ -7,9 +8,11 @@ const filters = {
 	"Domain": ["Energy", "Climate"]
 }
 
-const FilterBar = ({ query, setQuery }) => {
+const FilterBar = ({filterResults}) => {
+	const [selectedFilters, setSelectedFilters] = React.useState(null);
+
 	const applyFilter = (filter, selectedOptions) => {
-		// noop
+		setSelectedFilters((prev) => ({ ...prev, [filter]: selectedOptions }));
 	};
 
 	return (
@@ -23,7 +26,7 @@ const FilterBar = ({ query, setQuery }) => {
 				}
 			</div>
 			{/* Filter button */}
-			<button className="filter-button">
+			<button className="filter-button" onClick={() => filterResults(selectedFilters)}>
 				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
 					<path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2a1 1 0 01-.293.707l-6.414 6.414A1 1 0 0013 13.414V19a1 1 0 01-1.447.894l-2-1A1 1 0 019 18v-4.586a1 1 0 00-.293-.707L2.293 6.707A1 1 0 012 6V4z" />
 				</svg>
--- src/pages/SearchResultPage.js
+++ src/pages/SearchResultPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchComponent from "../components/SearchComponent";
 import { useNavigate } from "react-router-dom";
 import ClaimListCard from "../components/SearchListClaimCard";
@@ -11,12 +11,20 @@ const mockClaims = [
 	{
 		id: 1,
 		title: "Claim 1 similar to search topic",
+		chatType: "Bar Chart",
+		reasoningStrategy: "Contrastive Reasoning",
+		sourceOfEvidence: "Chart + Caption",
+		domain: "Climate",
 		caption:
 			"Caption here is Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus ex id elementum pharetra. Nam feugiat scelerisque orci, nec consectetur ipsum pellentesque ut. In interdum, leo a eleifend fringilla, felis erat porta elit, imperdiet tincidunt nunc felis sed dui. Donec at nisl aliquet, rhoncus lectus et, varius elit. Nunc lobortis mi at ornare aliquam. Quisque varius eleifend magna eu eleifend. Fusce ut eros sit amet mauris dapibus pulvinar tempus maximus lectus. Pellentesque scelerisque blandit nisl, in feugiat velit bibendum ut. Aliquam erat volutpat. Donec in euismod eros, non pulvinar leo.",
 	},
 	{
 		id: 2,
 		title: "Claim 2 similar to search topic",
+		chatType: "Line Chart",
+		reasoningStrategy: "Causal Inference",
+		sourceOfEvidence: "Chart + Caption",
+		domain: "Energy",
 		caption:
 			"Caption here is Cras egestas elementum quam, sed accumsan neque accumsan et. In non lacus pretium, suscipit nibh vel, tincidunt lacus. Fusce quis tincidunt nunc, convallis consequat mi. Pellentesque dictum quam at dignissim rhoncus. Nullam sagittis, nibh eu tincidunt maximus, arcu elit ultrices justo, quis ultrices velit risus consequat leo. Nunc a pharetra orci. Quisque lobortis ligula in tortor porta dapibus. Proin ac augue metus. Fusce sollicitudin vitae orci vitae vulputate. Morbi sit amet metus a neque aliquam egestas vitae in mauris. Morbi aliquet tincidunt efficitur. Praesent tempor tellus id ex semper, bibendum imperdiet ante aliquet. Donec at aliquet quam, nec tristique quam. In hac habitasse platea dictumst. Quisque consectetur dictum ultrices. Proin at mi nec metus dapibus pharetra a at felis. Sed in congue enim, a lobortis orci. Etiam venenatis sollicitudin urna, ut fermentum erat tempor non. Nullam lorem est, vestibulum non felis in, tincidunt imperdiet lacus. Nunc neque nisl, cursus in tempus non, sollicitudin tempor elit. Suspendisse ornare ipsum vel tellus dapibus, ut facilisis neque lacinia. Cras hendrerit lectus nibh, ut mollis purus scelerisque sed. Aliquam ullamcorper libero ex, a imperdiet nunc accumsan et. Donec porttitor quis tellus ac finibus. Curabitur non ornare lectus. Morbi aliquet molestie ligula, sit amet iaculis ante pharetra non. Vivamus porta turpis tellus, vitae aliquam orci lacinia id. Morbi eu orci maximus, tempor justo vel, mattis neque. Donec bibendum egestas tortor ac consectetur.",
 
@@ -24,6 +32,10 @@ const mockClaims = [
 	{
 		id: 3,
 		title: "Claim 3 similar to search topic",
+		chatType: "Pie Chart",
+		reasoningStrategy: "Corroboration",
+		sourceOfEvidence: "Chart Only",
+		domain: "Climate",
 		caption:
 			"Caption here is Donec consequat, tellus non interdum cursus, erat nisi posuere lectus, eget lobortis est neque a augue. Donec consectetur nibh nibh, id auctor nunc placerat ac. Pellentesque consequat mi nulla, in porttitor dui feugiat vel. Proin volutpat, neque eu consectetur efficitur, urna erat lobortis justo, a rhoncus purus ipsum eu dolor. Nam facilisis interdum efficitur. Etiam et condimentum ligula. Nam porttitor, nulla nec ullamcorper ultrices, ipsum lorem tempor felis, vitae suscipit leo metus porta ante. Aliquam suscipit iaculis iaculis.",
 
@@ -31,12 +43,20 @@ const mockClaims = [
 	{
 		id: 4,
 		title: "Claim 4 similar to search topic",
+		chatType: "Scatter Plot",
+		reasoningStrategy: "Contrastive Reasoning",
+		sourceOfEvidence: "Caption Only",
+		domain: "Energy",
 		caption:
 			"Caption here is Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus ex id elementum pharetra. Nam feugiat scelerisque orci, nec consectetur ipsum pellentesque ut. In interdum, leo a eleifend fringilla, felis erat porta elit, imperdiet tincidunt nunc felis sed dui. Donec at nisl aliquet, rhoncus lectus et, varius elit. Nunc lobortis mi at ornare aliquam. Quisque varius eleifend magna eu eleifend. Fusce ut eros sit amet mauris dapibus pulvinar tempus maximus lectus. Pellentesque scelerisque blandit nisl, in feugiat velit bibendum ut. Aliquam erat volutpat. Donec in euismod eros, non pulvinar leo.",
 	},
 	{
 		id: 5,
 		title: "Claim 5 similar to search topic",
+		chatType: "Area Chart",
+		reasoningStrategy: "Triangulation",
+		sourceOfEvidence: "Chart + Caption",
+		domain: "Climate",
 		caption:
 			"Caption here is Cras egestas elementum quam, sed accumsan neque accumsan et. In non lacus pretium, suscipit nibh vel, tincidunt lacus. Fusce quis tincidunt nunc, convallis consequat mi. Pellentesque dictum quam at dignissim rhoncus. Nullam sagittis, nibh eu tincidunt maximus, arcu elit ultrices justo, quis ultrices velit risus consequat leo. Nunc a pharetra orci. Quisque lobortis ligula in tortor porta dapibus. Proin ac augue metus. Fusce sollicitudin vitae orci vitae vulputate. Morbi sit amet metus a neque aliquam egestas vitae in mauris. Morbi aliquet tincidunt efficitur. Praesent tempor tellus id ex semper, bibendum imperdiet ante aliquet. Donec at aliquet quam, nec tristique quam. In hac habitasse platea dictumst. Quisque consectetur dictum ultrices. Proin at mi nec metus dapibus pharetra a at felis. Sed in congue enim, a lobortis orci. Etiam venenatis sollicitudin urna, ut fermentum erat tempor non. Nullam lorem est, vestibulum non felis in, tincidunt imperdiet lacus. Nunc neque nisl, cursus in tempus non, sollicitudin tempor elit. Suspendisse ornare ipsum vel tellus dapibus, ut facilisis neque lacinia. Cras hendrerit lectus nibh, ut mollis purus scelerisque sed. Aliquam ullamcorper libero ex, a imperdiet nunc accumsan et. Donec porttitor quis tellus ac finibus. Curabitur non ornare lectus. Morbi aliquet molestie ligula, sit amet iaculis ante pharetra non. Vivamus porta turpis tellus, vitae aliquam orci lacinia id. Morbi eu orci maximus, tempor justo vel, mattis neque. Donec bibendum egestas tortor ac consectetur.",
 
@@ -44,6 +64,10 @@ const mockClaims = [
 	{
 		id: 6,
 		title: "Claim 6 similar to search topic",
+		chatType: "Heatmap",
+		reasoningStrategy: "Contrastive Reasoning",
+		sourceOfEvidence: "Chart Only",
+		domain: "Energy",
 		caption:
 			"Caption here is Donec consequat, tellus non interdum cursus, erat nisi posuere lectus, eget lobortis est neque a augue. Donec consectetur nibh nibh, id auctor nunc placerat ac. Pellentesque consequat mi nulla, in porttitor dui feugiat vel. Proin volutpat, neque eu consectetur efficitur, urna erat lobortis justo, a rhoncus purus ipsum eu dolor. Nam facilisis interdum efficitur. Etiam et condimentum ligula. Nam porttitor, nulla nec ullamcorper ultrices, ipsum lorem tempor felis, vitae suscipit leo metus porta ante. Aliquam suscipit iaculis iaculis.",
 
@@ -52,7 +76,33 @@ const mockClaims = [
 
 const SearchResultPage = ({ query, setQuery }) => {
 	const navigate = useNavigate();
+	const [displayedClaims, setDisplayedClaims] = React.useState(mockClaims);
 	const [selectedView, setSelectedView] = React.useState("list");
+	const [appliedFilters, setAppliedFilters] = React.useState({});
+
+	useEffect(() => {
+		// filter mocklaims based on appliedFilters
+		let filtered = mockClaims;
+		Object.keys(appliedFilters).forEach((filterKey) => {
+			const selectedOptions = appliedFilters[filterKey];
+			if (selectedOptions && selectedOptions.length > 0) {
+				filtered = filtered.filter((claim) =>
+					selectedOptions.includes(
+						filterKey === "Chart type"
+							? claim.chatType
+							: filterKey === "Reasoning Strategy"
+								? claim.reasoningStrategy
+								: filterKey === "Source of Evidence"
+									? claim.sourceOfEvidence
+									: filterKey === "Domain"
+										? claim.domain
+										: ""
+					)
+				);
+			}
+		});
+		setDisplayedClaims(filtered);
+	}, [appliedFilters]);
 
 	const handleSearch = (q) => {
 		if (q) {
@@ -70,11 +120,10 @@ const SearchResultPage = ({ query, setQuery }) => {
 					<SearchComponent onSearch={handleSearch} query={query} setQuery={setQuery} />
 					<div className="flex border border-gray-400 rounded-full overflow-hidden">
 						<button
-							className={`flex items-center px-3 py-1 transition-colors ${
-								selectedView === "list"
+							className={`flex items-center px-3 py-1 transition-colors ${selectedView === "list"
 									? "bg-blue-100 text-blue-900"
 									: "bg-white text-gray-800"
-							}`}
+								}`}
 							onClick={() => setSelectedView("list")}
 							aria-label="List view"
 							type="button"
@@ -82,11 +131,10 @@ const SearchResultPage = ({ query, setQuery }) => {
 							<ListIcon />
 						</button>
 						<button
-							className={`flex items-center px-3 py-1 transition-colors border-l border-gray-400 ${
-								selectedView === "grid"
+							className={`flex items-center px-3 py-1 transition-colors border-l border-gray-400 ${selectedView === "grid"
 									? "bg-blue-100 text-blue-900"
 									: "bg-white text-gray-800"
-							}`}
+								}`}
 							onClick={() => setSelectedView("grid")}
 							aria-label="Grid view"
 							type="button"
@@ -102,11 +150,12 @@ const SearchResultPage = ({ query, setQuery }) => {
 				</p>
 
 				{/* Filter bar */}
-				<FilterBar />
+				<FilterBar filterResults={setAppliedFilters} />
 
 				{/* Claims list */}
-				<section className={selectedView === "list" ? "mt-3 space-y-3" : "mt-3 grid grid-cols-3 gap-3"}>
-						{mockClaims.map((c, idx) => (
+				{displayedClaims.length === 0 ? (<div className="text-gray-500">No claims to display</div>) : (
+					<section className={selectedView === "list" ? "mt-3 space-y-3" : "mt-3 grid grid-cols-3 gap-3"}>
+						{displayedClaims.map((c, idx) => (
 							<div key={c.id} className="space-y-3 bg-white shadow-sm">
 								{selectedView === "list" ? (
 									<ClaimListCard claim={c} />
@@ -115,7 +164,8 @@ const SearchResultPage = ({ query, setQuery }) => {
 								)}
 							</div>
 						))}
-				</section>
+					</section>
+				)}
 			</main>
 		</div>
 	);
